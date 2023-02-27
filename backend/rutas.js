2546742fb3198e9e0d1114fa8ba8fa2f6edaa1a6
Enma04 <programmer1ebg@gmail.com>
2023-02-27 01:01:05 -0500

    Versión 3.4: Ajuste visual del código

--- backend/rutas.js
+++ backend/rutas.js
@@ -1,30 +1,34 @@
 /*
- Este archivo lo único que hace es crear las api's que se van a consumir, con
- su respectiva dirección de la pgáina web para hacer las peticiones (dirección)
- y utiliza la información proveniente de "controladoresUsuarios.js"
- ESTE ARCHIVO PUEDE IR PERFECTAMENTE EN "app.js"
+    Este archivo lo único que hace es crear las api's que se van a consumir, con
+    su respectiva dirección de la pgáina web para hacer las peticiones (dirección)
+    y utiliza la información proveniente de "controladoresUsuarios.js"
+    ESTE ARCHIVO PUEDE IR PERFECTAMENTE EN "app.js"
 */
 
 const { response } = require("express");
 
-var usuariosRutas = require(__dirname + "/controladores/controladorUsuarios.js").controladorUsuariosExport
+var usuariosRutas = require(__dirname +
+  "/controladores/controladorUsuarios.js").controladorUsuariosExport;
 
 //Middlewear valida que la sesión esté activa para usar alguna appi
 var validarSesion = function (peticion, respuesta, next) {
-    if (peticion.session._id == undefined || peticion.session._id == "" || peticion.session._id == null || peticion.session._id == " ") {
-        respuesta.json({ state: false, mensaje: "Su sesión ha caducado, por favor ingrese nuevamente", redireccion:true });
-        return false;
-    }
-    else {
-        next();
-        return true;
-    }
-}
-
-
-
-
-
+  if (
+    peticion.session._id == undefined ||
+    peticion.session._id == "" ||
+    peticion.session._id == null ||
+    peticion.session._id == " "
+  ) {
+    respuesta.json({
+      state: false,
+      mensaje: "Su sesión ha caducado, por favor ingrese nuevamente",
+      redireccion: true,
+    });
+    return false;
+  } else {
+    next();
+    return true;
+  }
+};
 
 /*
 ---------------------//---------------------------------//-------------- 
@@ -34,65 +38,64 @@ var validarSesion = function (peticion, respuesta, next) {
 
 // Api CREATE
 app.post("/Cliente/Guardar", function (peticion, respuesta) {
-    usuariosRutas.Guardar(peticion, respuesta);
-})
+  usuariosRutas.Guardar(peticion, respuesta);
+});
 
 // Api READ
-app.post("/Cliente/ListarUsuarios", /*validarSesion, */function (peticion,respuesta) {
+app.post(
+  "/Cliente/ListarUsuarios",
+  /*validarSesion, */ function (peticion, respuesta) {
     usuariosRutas.ListarUsuarios(peticion, respuesta);
-})
+  }
+);
 
 // Api UPDATE
 app.post("/Cliente/Modificar", function (peticion, respuesta) {
-    usuariosRutas.Modificar(peticion, respuesta);
-})
+  usuariosRutas.Modificar(peticion, respuesta);
+});
 
 // Api DELETE
 app.post("/Cliente/Eliminar", function (peticion, respuesta) {
-    usuariosRutas.Eliminar(peticion, respuesta);
-})
-
-
-
-
-
-
+  usuariosRutas.Eliminar(peticion, respuesta);
+});
 
 //---------------------------------------------------------------------------------------
 // API'S ADICIONALES
 //---------------------------------------------------------------------------------------
 
 // Api READ de 1 usuario
-app.post("/Cliente/ListarUsuario", function (peticion,respuesta) {
-    usuariosRutas.ListarUsuario(peticion, respuesta);
-})
+app.post("/Cliente/ListarUsuario", function (peticion, respuesta) {
+  usuariosRutas.ListarUsuario(peticion, respuesta);
+});
 
 // Api Login (no se usa)
-app.post("/Cliente/Login", function (peticion,respuesta) {
-    usuariosRutas.Login(peticion, respuesta);
-})
+app.post("/Cliente/Login", function (peticion, respuesta) {
+  usuariosRutas.Login(peticion, respuesta);
+});
 
 // Api Login usuario normal
-app.post("/Cliente/LoginUsuario", function (peticion,respuesta) {
-    usuariosRutas.LoginUsuario(peticion, respuesta);
-})
+app.post("/Cliente/LoginUsuario", function (peticion, respuesta) {
+  usuariosRutas.LoginUsuario(peticion, respuesta);
+});
 
 // Api ver cookies de sesion
-app.post("/Cliente/MostrarCookies", function (peticion,respuesta) {
-    usuariosRutas.MostrarCookies(peticion, respuesta);
-})
+app.post("/Cliente/MostrarCookies", function (peticion, respuesta) {
+  usuariosRutas.MostrarCookies(peticion, respuesta);
+});
 
 // Api para cerrar sesion
-app.post("/Cliente/CerrarSesion", function (peticion,respuesta) {
-    usuariosRutas.CerrarSesion(peticion, respuesta);
-})
+app.post("/Cliente/CerrarSesion", function (peticion, respuesta) {
+  usuariosRutas.CerrarSesion(peticion, respuesta);
+});
 
 // Api para definir opciones del menú lateral
-app.post("/Cliente/MenuDefinido", validarSesion, function (peticion,respuesta) {
+app.post(
+  "/Cliente/MenuDefinido",
+  validarSesion,
+  function (peticion, respuesta) {
     usuariosRutas.MenuDefinido(peticion, respuesta);
-})
-
-
+  }
+);
 
 //--------------------------------------------------------------------------
 //PRUEBAS PARA VER EN POSTMAN
