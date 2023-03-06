2c6e6f73160a406371b9e98e9dc3f16802ac6211
Enma04 <programmer1ebg@gmail.com>
2023-03-06 01:35:34 -0500

    Versión 4.9: Se añadió una zona para agregar servicios de forma administrativa

--- frontendAngular/src/app/MisComponentes/read-servicios/read-servicios.component.ts
+++ frontendAngular/src/app/MisComponentes/read-servicios/read-servicios.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
+
 @Component({
   selector: 'app-read-servicios',
   templateUrl: './read-servicios.component.html',
   styleUrls: ['./read-servicios.component.css']
 })
 export class ReadServiciosComponent {
+
+//--------------------------------------------------------------------
+//VARIABLES DE LA CLASE
+//--------------------------------------------------------------------
+
+  codigo!: number;
+  nombre: string = "";
+  precio!: number;
+
+  check: boolean = false;
+  DatosPrpductos: any[] = [];
+
+
+
+//--------------------------------------------------------------------
+//FUNCIONES DE LA CLASE
+//--------------------------------------------------------------------
+
+  //Función para validar la aceptación de los términos y condiciones
+  Terminos() {
+    this.check = !this.check;
+  } //Fin Función Terminos()
+
+  //Función qque caga la ventana modal
+  CargarModal() {
+    $('#exampleModalCenter').modal('show');
+  }
 
 }
